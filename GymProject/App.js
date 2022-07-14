7f02fb3cdc3a3b1c0682fff27726f9f8809be5ea
Andrewjsy <kj10522002@gmail.com>
2022-07-14 22:25:03 +0900

    로그인페이지 구상
    App.js에서 바로 로그인페이지로 들어가게 변경 (작업 용이하게 하기위해 임시로 변경, 로그인페이지 완성 후 다시 복구할 예정), 기존 main화면이 필요하면 주석 처리된 24~35의 주석을 해제하고 36~39까지 주석처리해서 사용하면됩니당.. 빠르게 완성해서 복구해놓을게요

--- GymProject/App.js
+++ GymProject/App.js
@@ -5,8 +5,10 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -19,6 +21,7 @@ const Tab = createBottomTabNavigator();
 
 const App = () => {
   return (
+    /*
     <NavigationContainer>
       <Tab.Navigator
         initialRouteName="Home"
@@ -29,9 +32,51 @@ const App = () => {
         <Tab.Screen name="Setting" component={SettingScreen} />
       </Tab.Navigator>
     </NavigationContainer>
+    */
+    <SafeAreaView style={{flex:1}}>
+        <View style={{flex:1, alignItems : 'center'}}>
+            <View style={styles.block1}>
+                <Text style={styles.text}>Log In</Text>
+            </View>
+            <View style={styles.inputblock}>
+                <TextInput placeholder="ID를 입력하세요." style={styles.input} />
+            </View>
+            <View style={{height:20}}/>
+            <View style={styles.inputblock}>
+                <TextInput placeholder="비밀번호를 입력하세요." style={styles.input} />
+            </View>
+            <View style={{height : 40, marginTop : 40, flexDirection : 'row'}}>
+                <Button title="로그인" color='#bdbdbd'/>
+                <View style={{width:'30%'}}/>
+                <Button title="회원가입" color='#bdbdbd'/>
+            </View>
+            <View style={{height : 40, marginTop : 40, flexDirection : 'row', alignItems:'center'}}>
+                <Text style={{fontSize:20,fontStyle:'italic'}}>관리자이신가요?</Text>
+                <View style={{width : '15%'}}/>
+                <Button title="관리자로 로그인"/>
+            </View>
+        </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create(); //수정예정
+const styles = StyleSheet.create({
+    block1 : {flex:0.7,
+    alignItems: 'center',
+    justifyContent:'center',},
+    inputblock : {
+    height : '6%',
+    width : '90%',
+    paddingHorizontal: 15,
+    borderColor:'#bdbdbd',
+    borderTopWidth:1,
+    borderBottomWidth:1,
+    borderRightWidth:1,
+    borderLeftWidth:1,
+    justifyContent : 'center',
+    },
+    text : {fontSize:48, fontWeight:'bold', color:'black'},
+    input : {fontSize : 20, paddingVertical:8,},
+}); //수정예정
 
 export default App;
