bd13df8863cadb2c824d2e00bffcda8b3e322adf
Andrewjsy <kj10522002@gmail.com>
2022-08-19 00:18:54 +0900

    0819commit
    HomeScreen.js 초안 commit

--- GymProject/screens/HomeScreen.js
+++ GymProject/screens/HomeScreen.js
@@ -5,8 +5,21 @@ import HomeScreen1 from './HomeScreen1.js';
 import HomeScreen2 from './HomeScreen2.js';
 
 const HomeScreen = ({navigation, route}) => {
-  const [casenum, setCaseNum] = useState(2);
-  //casenum = 0: 이용중인기구있음, 1: 이용중인기구없음&이용가능기구존재, 2: 예약내역없음&이용가능기구없음
+  const [casenum, setCaseNum] = useState(0);
+  const checkuser=()=>{
+  //석우꺼-user id&헬스장 id 넘겨주고 user의 이용정보 리스트 json으로 받아오기
+  //받아와야하는 정보는 다음과 같다 :
+  //1. 이용중인 기구의 id, name, 이용시작시간 (이용중이지 않을경우 3개 다 null혹은 -1값)
+  //2. 이용가능한 기구의 id, name, 이용가능시작시간 (이용가능한 기구가 없을 경우 3개 다 null혹은 -1값)
+  //-> 총 6개 변수 필요
+    return;
+  };
+  const casify=()=>{
+  //내꺼-> checkuser()의 리스트보고 0,1,2 중 하나로 return
+    return;
+  }
+  //setCaseNum(checkuser()); -> 이부분에서 infinite loop 에러남, 해결해야할듯.
+  //casenum = 0: 이용중인기구있음, 1: 이용중인기구없음&이용가능기구존재, 2: 이용중인기구없음&이용가능기구없음
   if (casenum === 0){ //이용중인기구있음
     return (
      <SafeAreaView style={styles.baseview}>
--- GymProject/screens/HomeScreen2.js
+++ GymProject/screens/HomeScreen2.js
@@ -4,13 +4,14 @@ import {Text, View, Button, StyleSheet, SafeAreaView} from 'react-native';
 const HomeScreen2 = () =>{
     return(
         <View style={styles.baseview}>
-        <Text>현재 이용가능한 기구가 없습니다.</Text>
+            <Text style={styles.text}>현재 이용가능한 기구가 없습니다</Text>
+            <View style={{flex:0.4}}/>
         </View>
     );}
 
 const styles = StyleSheet.create({
   baseview: {flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white'},
-  text:{},
+  text: {fontSize: 25, fontWeight: 'bold', textAlign: 'center'},
 });
 
 export default HomeScreen2;
