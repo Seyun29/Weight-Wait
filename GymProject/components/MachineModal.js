6d896cd5dddad69824b44a255f467186b0336b1e
offtheglass <86214286+offtheglass@users.noreply.github.com>
2022-08-29 17:00:18 +0900

    29/8/17:00

--- GymProject/components/MachineModal.js
+++ GymProject/components/MachineModal.js
@@ -12,13 +12,64 @@ import AsyncStorage from '@react-native-async-storage/async-storage'; // add thi
 
 const MachineModal = ({name, id, waitnum}) => {
   const formatted = `기구명 : ${name}\n\n현재 대기인원 : ${waitnum}명`;
+  const [userid,setUserid]=useState('');
+  const getusername = async () => {
+    
+    try{const uid=await AsyncStorage.getItem('@storage_userid');
+    setUserid(uid);
+    return }
+    catch(e){
+        console.log(e);
+    }
+  }
+  getusername(); 
+
+  const machinereturn= (machineid,usid) => {  // machinereturn함수 machineid는 반납하려는 머신id주면되고, usid는 userid인데 위에 선언했으니깐 userid 쓰면 됨
+                                              // 제대로 반납되면 1, 반납 안 되면 -1 리턴
+    try{
+    fetch(
+      'https://so6wenvyg8.execute-api.ap-northeast-2.amazonaws.com/dev/return',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          machineid: String(machineid),
+          userid: String(usid),
+        }),
+      },
+    )
+    .then(response => response.json())
+    .then(json => {
+      console.log(json);
+      try{
+        tim=json["time"];
+        Alert.alert("반납되었습니다.");
+        return 1;  
+    }
+      catch(e){
+        Alert.alert("반납에 실패했습니다.") 
+        return -1
+
+      }
+    })
+  }
+  catch(e){
+    console.log('17');
+    console.log(e);
+  }
+   }
+
+
   return (
     <View style={styles.machine}>
       <Image
         source={require('../images/default_image.png')}
         style={{width: 70, height: 70}}></Image>
       <Text>{formatted}</Text>
-      <Button title="예약취소"></Button>
+      <Button title="예약취소" onPress={() =>machinereturn(id,userid) }></Button>
     </View>
   );
 };
